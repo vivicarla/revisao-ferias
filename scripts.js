2625357dd6d6e8f2250e1aa7e71832a5a0616502
Carla Viviane <carla_dourado@estudante.sesisenai.org.br>
2026-02-23 17:05:44 -0300

    .

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-alert("teste")
+const texto = document.getElementById("texto")
+const botao = document.getElementById("botao")
+const senha = "nahida1"
+
+
+botao.addEventListener("click",()=>{
+    const cupom = document.getElementById("cupom").value
+    if (cupom==senha){
+        texto.textContent="você ganhou 10% de desconto em sua compra."
+        botao.textContent="resgatado"
+    }else{
+        texto.textContent="Código inválido"
+    }
+    
+});
